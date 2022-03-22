5f5f8fc82dc55afe70d5a9c6dd2841950bd841dc
prabhv <prabhan.v.naik@gmail.com>
2022-03-22 20:32:31 +0100

    Initial commit

---
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,21 @@
+package com.example.demo.student;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    public List<Student> getStudents(){
+        return List.of(
+                new Student(1L,
+                        "Miriam",
+                        28,
+                        LocalDate.of(2000, 11, 11),
+                        "email@email.com")
+        );
+    }
+}
