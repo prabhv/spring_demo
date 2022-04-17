213b9b2d337fc01ffd55d5c5eabd56c9f8b02134
prabhv <prabhan.v.naik@gmail.com>
2022-04-17 10:13:09 +0200

    Junit tests for StudentService using Mockito to mock repository response

--- src/test/java/com/example/demo/student/StudentServiceTest.java
+++ src/test/java/com/example/demo/student/StudentServiceTest.java
@@ -1,12 +1,22 @@
 package com.example.demo.student;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 
+@ExtendWith(MockitoExtension.class)
 class StudentServiceTest {
 
     @Mock
@@ -16,13 +26,7 @@ class StudentServiceTest {
 
     @BeforeEach
     void setUp() {
-        autoCloseable = MockitoAnnotations.openMocks(this);
         underTest = new StudentService(studentRepository);
-    }
-
-    @AfterEach
-    void tearDown() throws Exception {
-        autoCloseable.close();
     }
 
     @Test
@@ -35,8 +39,37 @@ class StudentServiceTest {
     }
 
     @Test
-    @Disabled
-    void addNewStudent() {
+    void canAddNewStudent() {
+        //given
+        String email = "happy@email.com";
+        Student student = new Student("Happy", LocalDate.of(2006, 7, 29), email);
+
+        //when
+        underTest.addNewStudent(student);
+
+        //then
+        ArgumentCaptor<Student> studentArgumentCaptor = ArgumentCaptor.forClass(Student.class);
+
+        verify(studentRepository).save(studentArgumentCaptor.capture());
+        Student capturedStudent = studentArgumentCaptor.getValue();
+
+        assertThat(capturedStudent).isEqualTo(student);
+
+    }
+
+    @Test
+    void throwExcWhenCannotAddNewStudent() {
+        //given
+        String email = "happy@email.com";
+        Student student = new Student("Happy", LocalDate.of(2006, 7, 29), email);
+
+        given(studentRepository.findStudentByEmail(student.getEmail())).willReturn(Optional.of(student));
+        //when
+        //then
+        assertThatThrownBy(() -> underTest.addNewStudent(student))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("Email is taken");
+
     }
 
     @Test
